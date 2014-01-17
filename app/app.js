82bcb1e06ba402cc363e164b4c486efd490f9922
rleivaAlivebox <rleiva@alivebox.com>
2014-01-17 11:32:15 -0600

    create repository

---
+++ app/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Richard
+ * Date: 17/01/14
+ * Time: 09:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+'use strict';
+
+var personApp = angular.module('personApp',[
+    'ngRoute'
+]);
+
+personApp.config(['$routeProvider',
+    function($routeProvider) {
+    }]);
